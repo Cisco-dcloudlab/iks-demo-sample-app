e085002526311c39247d889e66a82462ac3c805d
andrewwa-cisco <87471918+andrewwa-cisco@users.noreply.github.com>
2021-08-09 11:05:02 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ variable "ikswsname" {
 resource helm_release helloiksfrtfcb {
   name       = "helloiksapp"
   namespace = "default"
-  chart = "https://prathjan.github.io/helm-chart/helloiks-0.1.0.tgz"
+  chart = "http://100.64.0.15/helloiks-0.1.0.tgz"
 
   set {
     name  = "MESSAGE"
